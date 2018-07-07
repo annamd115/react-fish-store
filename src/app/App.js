b86f2828ac041a9d73ea825f249e0155a7bd6011
Anna Dolan <anna.dolan115@gmail.com>
2018-07-07 10:55:48 -0500

    added home component routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 import './App.css';
-import SingleOrder from '../components/SingleOrder/SingleOrder';
-import Register from '../components/Register/Register';
-import OrderSpa from '../components/OrderSpa/OrderSpa';
-import Order from '../components/Order/Order';
-import New from '../components/New/New';
+// import SingleOrder from '../components/SingleOrder/SingleOrder';
+// import Register from '../components/Register/Register';
+// import OrderSpa from '../components/OrderSpa/OrderSpa';
+// import Order from '../components/Order/Order';
+// import New from '../components/New/New';
 import Navbar from '../components/Navbar/Navbar';
-import Login from '../components/Login/Login';
-import Inventory from '../components/Inventory/Inventory';
+// import Login from '../components/Login/Login';
+// import Inventory from '../components/Inventory/Inventory';
 import Home from '../components/Home/Home';
-import Fish from '../components/Fish/Fish';
-
-
+// import Fish from '../components/Fish/Fish';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Fish />
-        <Home />
-        <Inventory />
-        <Login />
-        <Navbar />
-        <New />
-        <Order />
-        <OrderSpa />
-        <Register />
-        <SingleOrder />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
+
       </div>
     );
   }
