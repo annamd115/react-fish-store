7cf0d040b2c39859b7fa0e24b56ade0a342a2a33
Anna Dolan <anna.dolan115@gmail.com>
2018-07-07 11:22:20 -0500

    routing for inventory component

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
 // import Register from '../components/Register/Register';
@@ -8,11 +8,32 @@ import './App.css';
 // import New from '../components/New/New';
 import Navbar from '../components/Navbar/Navbar';
 // import Login from '../components/Login/Login';
-// import Inventory from '../components/Inventory/Inventory';
+import Inventory from '../components/Inventory/Inventory';
 import Home from '../components/Home/Home';
 // import Fish from '../components/Fish/Fish';
 
+const PrivateRoute = ({ component : Component, authed, ...rest}) => {
+  return (
+    <Route 
+      {...rest}
+      render={props => 
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect 
+            to={{pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state={
+    authed: false,
+  }
+
   render() {
     return (
       <div className="App">
@@ -23,6 +44,11 @@ class App extends Component {
               <div className="row">
                 <Switch>
                   <Route path="/" exact component={Home}/>
+                  <PrivateRoute 
+                    path="/inventory" 
+                    authed={this.state.authed}
+                    component={Inventory}
+                  />
                 </Switch>
               </div>
             </div>
