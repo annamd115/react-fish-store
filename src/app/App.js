4256f0d26c30f4cd401a6870e208e58b27e3e439
Anna Dolan <anna.dolan115@gmail.com>
2018-07-07 14:03:42 -0500

    added routing for logout and login

--- src/app/App.js
+++ src/app/App.js
@@ -4,16 +4,14 @@ import firebase from 'firebase';
 
 import './App.css';
 
-// import SingleOrder from '../components/SingleOrder/SingleOrder';
+import SingleOrder from '../components/SingleOrder/SingleOrder';
 import Register from '../components/Register/Register';
-// import OrderSpa from '../components/OrderSpa/OrderSpa';
-// import Order from '../components/Order/Order';
-// import New from '../components/New/New';
+import OrderSpa from '../components/OrderSpa/OrderSpa';
+import New from '../components/New/New';
 import Navbar from '../components/Navbar/Navbar';
 import Login from '../components/Login/Login';
 import Inventory from '../components/Inventory/Inventory';
 import Home from '../components/Home/Home';
-// import Fish from '../components/Fish/Fish';
 import fbConnection from '../firebaseRequests/connection';
 fbConnection();
 
@@ -71,6 +69,10 @@ class App extends Component {
     this.removeListener();
   }
 
+  runAway = () => {
+    this.setState({authed:false});
+  }
+
   render() {
     return (
       <div className="App">
@@ -78,6 +80,7 @@ class App extends Component {
           <div>
             <Navbar 
               authed={this.state.authed}
+              runAway={this.runAway}
             />
             <div className="container">
               <div className="row">
@@ -97,6 +100,21 @@ class App extends Component {
                     path="/login" 
                     authed={this.state.authed}
                     component={Login}
+                  />
+                  <PrivateRoute 
+                    path="/orders" 
+                    authed={this.state.authed}
+                    component={OrderSpa}
+                  />
+                  <PrivateRoute 
+                    path="/order/:id" 
+                    authed={this.state.authed}
+                    component={SingleOrder}
+                  />
+                  <PrivateRoute 
+                    path="/new" 
+                    authed={this.state.authed}
+                    component={New}
                   />
                 </Switch>
               </div>
