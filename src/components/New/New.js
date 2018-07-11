e8e7044508b0a2a56f3a01e83e79f7b23acadc38
Anna Dolan <anna.dolan115@gmail.com>
2018-07-10 19:39:18 -0500

    saving new order to firebase

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -4,6 +4,9 @@ import Fish from '../Fish/Fish';
 import Order from '../Order/Order';
 
 import fishRequests from '../../firebaseRequests/fishes';
+import authRequests from '../../firebaseRequests/auth';
+import orderRequests from '../../firebaseRequests/orders';
+
 import './New.css';
 
 class New extends React.Component {
@@ -23,6 +26,20 @@ class New extends React.Component {
     delete newOrder[key];
     this.setState({order: newOrder});
   }
+
+  saveNewOrder = () => {
+    const newOrder = {fishes: {...this.state.order}};
+    newOrder.uid = authRequests.getUid();
+    newOrder.dateTime = Date.now();
+    orderRequests
+      .postRequest(newOrder)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch((err) => {
+        console.error('error in order post', err);
+      });
+  };
 
   componentDidMount () {
     fishRequests
@@ -58,6 +75,7 @@ class New extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           removeFromOrder={this.removeFromOrder}
+          saveNewOrder={this.saveNewOrder}
         />
       </div>
     );
